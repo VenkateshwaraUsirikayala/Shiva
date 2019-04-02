6a5ab903da8cfffb3a583bc16969ba483a468780
VenkateshwaraUsirikayala <usirikayalav@gmail.com>
2019-04-03 04:11:39 +0530

    commit

--- src/Task.java
+++ src/Task.java
@@ -3,6 +3,7 @@ public class Task {
 
 	public static void main(String[] args) {
 		System.out.println("sample project hi shiva good night. Changing file content to get merge conflict");
+		System.out.println("Hello");
 	}
 
 }
