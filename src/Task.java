536932d31e6a87749de5ab0f88c31dd956160f45
VenkateshwaraUsirikayala <usirikayalav@gmail.com>
2019-04-03 04:09:31 +0530

    Update Task.java

--- src/Task.java
+++ src/Task.java
@@ -2,7 +2,7 @@
 public class Task {
 
 	public static void main(String[] args) {
-		System.out.println("sample project hi shiva good night. Changing file content to get merge conflict");
+		System.out.println("sample project hi shiva good night. Getting merge conflict");
 	}
 
 }
