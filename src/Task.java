29287264ddc65fdb6fb9153d4f5baa63c2b7b2af
VenkateshwaraUsirikayala <usirikayalav@gmail.com>
2019-04-03 04:37:13 +0530

    shiva branch

--- src/Task.java
+++ src/Task.java
@@ -1,8 +1,6 @@
 
 public class Task {
-
 	public static void main(String[] args) {
-		System.out.println("sample projsjdfhashfoect hi shiva good night. Getting merge conflict");
+		System.out.println("sample projsjdfhashfoecnmmt hi shiva good night. Getting merge conflict");
 	}
-
 }
